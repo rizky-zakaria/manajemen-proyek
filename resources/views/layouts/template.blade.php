8ea0d352f2c8e0b6948a41e30c4516da49e0bb98
rizkyzakaria_ <rizky.zakaria78@gmail.com>
2022-11-08 02:38:36 +0800

    fix title

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Modules &rsaquo; DataTables &mdash; Stisla</title>
+    <title>Manajemen Proyek</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ asset('stisla/dist/assets/modules/bootstrap/css/bootstrap.min.css') }}">
