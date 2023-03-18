9b17124e70430b6e13d8eb2ce2a4a732d1b585ee
rizkyzakaria_ <rizky.zakaria78@gmail.com>
2023-03-18 21:19:22 +0800

    --allow-empty

--- app/Models/Gantt.php
+++ app/Models/Gantt.php
@@ -9,6 +9,6 @@ class Gantt extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'task_name', 'resource', 'tgl_mulai', 'bln_mulai', 'thn_mulai', 'tgl_selesai', 'bln_selesai', 'thn_selesai', 'duration', 'percent', 'dependencies', 'project_id'
+        'task_id', 'task_name', 'resource', 'tgl_mulai', 'bln_mulai', 'thn_mulai', 'tgl_selesai', 'bln_selesai', 'thn_selesai', 'duration', 'percent', 'dependencies', 'project_id'
     ];
 }
